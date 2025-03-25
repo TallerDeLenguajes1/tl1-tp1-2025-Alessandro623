394d8baf73e6d74bb55b7a0cead86ad40fe10b34
Alessandro623 <ordnasela_2010@hotmail.com.ar>
2025-03-25 15:27:27 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int numero = 0;
+    int *Pnumero;
+    Pnumero = &numero;
+
+    printf("contenido del puntero = %d\n", *Pnumero);
+    printf("direccion de memoria del almacend del puntero = %p\n", Pnumero);
+    printf("direccion de memoria del puntero = %p\n", &Pnumero);
+    printf("direccion de memoria de variable = %p\n", &numero);
+    printf("tamanio de memoria utlizado por la variable %zu", sizeof(numero));
+
+
 
     return 0;
 }
